c4f104e0d987281f424efc7e14f8f43c130f9dfd
Stephen Bennett <47852623+steviebdesignstraining@users.noreply.github.com>
2025-01-10 19:53:31 +0000

    video update
    updated with video's

--- pages/index.tsx
+++ pages/index.tsx
@@ -150,6 +150,38 @@ export default function Home() {
             />
             </div>
             </div>
+            <div className="basis-1/3 flex-1" >
+            <div style={{ padding: "64.95% 0 0 0", position: "relative" }}>
+              <iframe
+                src="https://player.vimeo.com/video/1004013752?h=63a102ec88"
+                style={{
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                  width: "100%",
+                  height: "100%"
+                }}
+                frameBorder={0}
+                allow="autoplay; fullscreen; picture-in-picture"
+              />
+            </div>
+            </div>
+            <div className="basis-1/3 flex-1" >
+            <div style={{ padding: "64.95% 0 0 0", position: "relative" }}>
+              <iframe
+                src="https://player.vimeo.com/video/1045752431?h=bbfaac50fb&am"
+                style={{
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                  width: "100%",
+                  height: "100%"
+                }}
+                frameBorder={0}
+                allow="autoplay; fullscreen; picture-in-picture"
+              />
+            </div>
+            </div>
             </div>
             <div>
             <h3 className="text-lg font-medium pt-8 pb-2 text-center text-teal-600">Designs</h3>
